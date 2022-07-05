cc0aebc16b99b7b30eb53997c1a4756e6e6ac2bd
ugrivous <36850433+ugrivous@users.noreply.github.com>
2022-07-05 18:50:14 +0200

    updated overall data structure, can now create and edit users, as well as add further calorie intake

--- main.py
+++ main.py
@@ -8,55 +8,42 @@ from collections import defaultdict
 from flask import Flask, render_template, jsonify, request
 
 ###### User Settings ########
-with open("Databases/user_database.txt","r") as u_d:
-    user_database = ast.literal_eval(u_d.read())
+try:
+    with open("Databases/user_database.json","r") as u_d:
+        user_database = json.load(u_d)
+except:
+    user_database = defaultdict()
+
 app = Flask(__name__)
-class UserProfile():
-    def __init__(self,name, age, weight, height, goal, sex, activity):
-        self.id = len(user_database) + 1
-        self.name = name
-        self.age = age
-        self.weight = weight
-        self.height = height
-        self.goal = goal
-        self.sex = sex
-        self.activity = activity # should be 1.2 , 1.5 or 1.7
-        self.kcal_intake = 0
-        self.calorie_goal = self.set_calorie_goal()
+def setUserProfile(name, age, weight, height, goal, sex, activity): #activity should be 1.2 , 1.5 or 1.7
+    args = locals()
+    new_user = {str(parameters):values for parameters, values in args.items()}
+    id = len(user_database.keys()) + 1
+    new_user["kcal_intake"] = 0
+    new_user["calorie_goal"] = set_calorie_goal(new_user)
+    return id , new_user
 
-    def update_user(self, parameters):
-        for parameter in parameters:
-            self.name = parameter[1]
-            self.age = parameter[2]
-            self.weight = parameter[3]
-            self.height = parameter[4]
-            self.goal = parameter[5]
-            self.sex = parameter[6]
-            self.kcal_intake = parameter[7]
-            self.calorie_goal = parameter[8]
+def set_calorie_goal(user):
+    if user["sex"] == "female":
+        bmr = (user["weight"] * 10.0 + user["height"] * 6.25 - user["age"] * 5.0 - 161.0) * user["activity"]
+    else:
+        bmr = (user["weight"] * 10.0 + user["height"] * 6.25 - user["age"] * 5.0 + 5.0) * user["activity"]
 
-    def set_calorie_goal(self):
-        if self.sex == "female":
-            bmr = (self.weight * 10.0 + self.height * 6.25 - self.age * 5.0 - 161.0) * self.activity
-        else:
-            bmr = (self.weight * 10.0 + self.height * 6.25 - self.age * 5.0 + 5.0) * self.activity
+    if user["goal"] == "loss":
+        goal = bmr - 500.0
+    elif user["goal"] == "gain":
+        goal = bmr + 500.0
+    else:
+        goal = bmr
+    return goal
 
-        if self.goal == "loss":
-            goal = bmr - 500.0
-        elif self.goal == "gain":
-            goal = bmr + 500.0
-        else:
-            goal = bmr
-        return goal
-
-    def toJSON(self):
-        return json.dumps(self, default=lambda o: o.__dict__,
-                          sort_keys=True)
+def toJSON(doc):
+    return json.dumps(doc, sort_keys=True)
 
 ####### Requests #########
 @app.get("/user_database")
-def get_countries():
-    user_jsonbase = [ json.loads(x.toJSON()) for x in user_database]
+def get_users():
+    user_jsonbase = user_database
     #print(user_jsonbase)
     return jsonify(user_jsonbase)
 
@@ -64,31 +51,53 @@ def get_countries():
 def create_user():
     if request.is_json:
         nur = request.get_json()
-        new_user = UserProfile(nur["name"], nur["age"], nur["weight"], nur["height"], nur["goal"], nur["sex"], nur["activity"])
-        user_database.append(new_user)
-        with open("Databases/user_database.txt", "w") as u_d:
-            u_d.write(str(user_database))
+        id , new_user = setUserProfile(nur["name"], nur["age"], nur["weight"], nur["height"], nur["goal"], nur["sex"], nur["activity"])
+        user_database[id] = new_user
+        with open("Databases/user_database.json", "w") as u_d:
+            u_d.write(json.dumps(user_database,indent=4))
         #response = requests.put("/Databases/user_database",json=new_user.toJSON())
-        return new_user.toJSON(), 201
+        return new_user, 201
     return {"TypeError:":"Request is not JSON"},415
+
+@app.put('/update_user_database')
+def update_user(): # gets user_id and parameters to be changed as request
+    if request.is_json:
+        req = request.get_json()
+        user= user_database[str(req["user_id"])]
+        print(f"------Updating {user['name']}------")
+        new_user = user
+        for key, value in req["parameters"].items():
+            new_user[key] = value
+        user = new_user
+        return user, 201
+    return {"TypeError:": "Request is not JSON"}, 415
+@app.put('/update_calories')
+def add_calories():
+    if request.is_json:
+        update_user()
+    return {"TypeError:": "Request is not JSON"}, 415
+
+@app.put('/user_selection')
+def change_user():
+    if request.is_json:
+        update_user()
+    return {"TypeError:": "Request is not JSON"}, 415
 
 ##### building database ########
 #TODO add database for users own products only
-class FoodItem():
-    def __init__(self, name, nutrients):
-        self.name = name
-        self.calorie_amount = nutrients
 def build_food_database(data_path):
-    print(f"------Building Databse-------")
-    kcal_database = defaultdict()
+    print(f"------Building Databse------")
+    food_database = defaultdict(dict)
+    tracked_nutrients = [1008,1003, 1005, 1004] # kcal,Protein, Carbohydrates,total Fat
     with open(data_path) as data:
         f = json.load(data)
         for fooditem in f["FoundationFoods"]:
-            kcal_database[(fooditem["description"].split(","))[0]] = list(filter(lambda x: x != '', [elem["amount"] if ('kcal' == elem["nutrient"]['unitName']) else '' for elem in fooditem["foodNutrients"]]))[0]
+            name = (fooditem["description"].split(","))[0]
+            food_database[name]= {elem["nutrient"]['name']: elem["amount"] for elem in fooditem["foodNutrients"]if elem["nutrient"]['id'] in tracked_nutrients}
 
-    with open("Databases/food_kcal_database.txt", "w") as kcal_saved:
-            kcal_saved.write(str(kcal_database))
-    return kcal_database
+    with open("Databases/food_kcal_database.json", "w") as kcal_saved:
+            kcal_saved.write(json.dumps(food_database,indent=4))
+    return food_database
 
 
 ###### System Settings #######
@@ -96,8 +105,8 @@ def initialize():
     if args["init"] == True:
         cal_data = build_food_database("FoodData_Central_foundation_food_json_2022-04-28.json")
     else:
-        with open("Databases/food_kcal_database.txt") as d:
-            cal_data = ast.literal_eval(d.read())
+        with open("Databases/food_kcal_database.json") as d:
+            cal_data = json.load(d)
     return cal_data
 
 if __name__ == '__main__':
@@ -109,8 +118,9 @@ if __name__ == '__main__':
         @app.route("/")
         def home():
             return render_template('bla.html')
-    app.run()
     food_database = initialize()
+    app.run()
+
     #active_scale = False
 
     #while active_scale:
